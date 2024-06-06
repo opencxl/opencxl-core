9a1749a2ce902e45c2b60885c757241995d5da11
Xiangqun Zhang <xzhang84@syr.edu>
2024-06-06 17:55:00 -0400

    Remove SingleLogicalDeviceConfig

--- opencxl/cxl/device/cxl_type1_device.py
+++ opencxl/cxl/device/cxl_type1_device.py
@@ -30,18 +30,6 @@ from opencxl.pci.component.config_space_manager import (
 )
 
 
-@dataclass
-class SingleLogicalDeviceConfig:
-    serial_number: str
-    port_index: int
-    memory_size: int  # in bytes
-    memory_file: str
-    vendor_id: int = EEUM_VID
-    device_id: int = SW_SLD_DID
-    subsystem_vendor_id: int = 0
-    subsystem_id: int = 0
-
-
 class CXLType1Device(RunnableComponent):
     def __init__(
         self,
