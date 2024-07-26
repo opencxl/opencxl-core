3d9fc8864e015c027b9c12d5bab7a2fdeb2e8ef5
Xiangqun Zhang <xzhang84@syr.edu>
2024-07-26 17:53:40 -0400

    Remove type hint

--- opencxl/cxl/component/virtual_switch/routers.py
+++ opencxl/cxl/component/virtual_switch/routers.py
@@ -417,7 +417,7 @@ class CxlCacheRouter(CxlRouter):
             if target_fld_name not in usp_component.get_cache_route_table_options():
                 logger.warning(self._create_message("Received unroutable CXL.cache packet"))
                 continue
-            target_port: int = usp_component.get_cache_route_table_options()[target_fld_name][
+            target_port = usp_component.get_cache_route_table_options()[target_fld_name][
                 "port_number"
             ]
             if target_port is None:
