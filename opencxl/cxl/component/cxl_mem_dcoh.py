3d6b87f9d8915628958b5a28ec0f630024c31915
sanghyukjung-eeum <sanghyuk.jung@eeum.io>
2024-08-01 18:32:24 -0700

    Fix for switch routing .mem drs packets (#85)
    Signed-off-by: Sanghyuk Jung <sanghyuk.jung@eeum.io>
    Signed-off-by: Kyeyoon Park <kyeyoon.park@eeum.io>

--- opencxl/cxl/component/cxl_mem_dcoh.py
+++ opencxl/cxl/component/cxl_mem_dcoh.py
@@ -106,7 +106,7 @@ class CxlMemDcoh(PacketProcessor):
         data: Optional[int] = 0,
         drs_opcode: Optional[CXL_MEM_S2MDRS_OPCODE] = CXL_MEM_S2MDRS_OPCODE.MEM_DATA,
         meta_field: Optional[CXL_MEM_META_FIELD] = CXL_MEM_META_FIELD.NO_OP,
-        meta_value: Optional[CXL_MEM_META_VALUE] = CXL_MEM_META_VALUE.ANY,
+        meta_value: Optional[CXL_MEM_META_VALUE] = CXL_MEM_META_VALUE.INVALID,
     ) -> Tuple[CxlMemCmpPacket, CxlMemMemDataPacket]:
         return (
             CxlMemCmpPacket.create(ndr_opcode, meta_field, meta_value),
@@ -190,11 +190,15 @@ class CxlMemDcoh(PacketProcessor):
         if data_flush is True:
             await self._memory_device_component.write_mem(addr, data)
 
-        ndr_packet, drs_packet = self._create_mem_rsp_packet(rsp_code, data)
-        await self._upstream_fifo.target_to_host.put(ndr_packet)
-
         if data_read is True:
+            ndr_packet, drs_packet = self._create_mem_rsp_packet(
+                rsp_code, data, meta_value=CXL_MEM_META_VALUE.ANY
+            )
+            await self._upstream_fifo.target_to_host.put(ndr_packet)
             await self._upstream_fifo.target_to_host.put(drs_packet)
+        else:
+            ndr_packet, _ = self._create_mem_rsp_packet(rsp_code, data)
+            await self._upstream_fifo.target_to_host.put(ndr_packet)
 
     # .mem m2s rwd (MemWr) handler
     async def _process_cxl_m2s_rwd_packet(self, m2srwd_packet: CxlMemM2SRwDPacket):
