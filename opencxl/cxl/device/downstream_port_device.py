c2bac21a9b0822aacadb17b205855852689e4e7c
Juhyung Park <qkrwngud825@gmail.com>
2024-10-24 14:14:31 +0900

    Implement SLD vPPB-PPB binding (#124)
    Co-authored-by: Chanwoo Moon <ixora990919@gmail.com>
    Signed-off-by: Chanwoo Moon <ixora990919@gmail.com>
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Kyeyoon Park <kyeyoon.park@eeum.io>

--- opencxl/cxl/device/downstream_port_device.py
+++ opencxl/cxl/device/downstream_port_device.py
@@ -5,7 +5,6 @@
  See LICENSE for details.
 """
 
-from dataclasses import dataclass
 from typing import Optional
 
 from opencxl.cxl.component.common import CXL_COMPONENT_TYPE
@@ -19,6 +18,7 @@ from opencxl.cxl.component.cxl_bridge_component import (
 )
 from opencxl.cxl.mmio import CombinedMmioRegister, CombinedMmioRegiterOptions
 from opencxl.cxl.device.port_device import CxlPortDevice
+from opencxl.cxl.device.pci_to_pci_bridge_device import PpbDevice
 from opencxl.cxl.config_space.port import (
     CxlDownstreamPortConfigSpace,
     CxlDownstreamPortConfigSpaceOptions,
@@ -38,7 +38,6 @@ from opencxl.pci.component.pci import (
     EEUM_VID,
     SW_DSP_DID,
 )
-from opencxl.pci.config_space.pci import REG_ADDR
 from opencxl.pci.component.mmio_manager import MmioManager, BarEntry
 from opencxl.pci.component.config_space_manager import (
     ConfigSpaceManager,
@@ -46,60 +45,39 @@ from opencxl.pci.component.config_space_manager import (
 )
 
 
-@dataclass
-class DummyConfig:
-    vcs_id: int
-    vppb_id: int
-    routing_table: RoutingTable
-
-
-@dataclass
-class EnumerationInfo:
-    secondary_bus: int
-    subordinate_bus: int
-    memory_base: int
-    memory_limit: int
-
-
 class DownstreamPortDevice(CxlPortDevice):
     def __init__(
         self,
         transport_connection: CxlConnection,
         port_index: int = 0,
-        dummy_config: Optional[DummyConfig] = None,
     ):
-        if dummy_config is not None:
-            self._vcs_id = dummy_config.vcs_id
-            self._vppb_index = dummy_config.vppb_id
-        else:
-            self._vppb_index: Optional[int] = None
-
         super().__init__(transport_connection, port_index)
 
-        self._dummy_config = dummy_config
-        self._is_dummy = dummy_config is not None
         self._pci_bridge_component = None
         self._pci_registers = None
         self._cxl_component = None
-        self._upstream_connection = CxlConnection()
+
+        self._ppb_device: PpbDevice = None
+        self._ppb_bind = None
+        self._vppb_index = -1
 
         self._cxl_io_manager = CxlIoManager(
-            self._upstream_connection.mmio_fifo,
-            self._transport_connection.mmio_fifo,
-            self._upstream_connection.cfg_fifo,
-            self._transport_connection.cfg_fifo,
+            self._vppb_upstream_connection.mmio_fifo,
+            self._vppb_downstream_connection.mmio_fifo,
+            self._vppb_upstream_connection.cfg_fifo,
+            self._vppb_downstream_connection.cfg_fifo,
             device_type=PCI_DEVICE_TYPE.DOWNSTREAM_BRIDGE,
             init_callback=self._init_device,
             label=self._get_label(),
         )
         self._cxl_mem_manager = CxlMemManager(
-            upstream_fifo=self._upstream_connection.cxl_mem_fifo,
-            downstream_fifo=transport_connection.cxl_mem_fifo,
+            upstream_fifo=self._vppb_upstream_connection.cxl_mem_fifo,
+            downstream_fifo=self._vppb_downstream_connection.cxl_mem_fifo,
             label=self._get_label(),
         )
         self._cxl_cache_manager = CxlCacheManager(
-            upstream_fifo=self._upstream_connection.cxl_cache_fifo,
-            downstream_fifo=transport_connection.cxl_cache_fifo,
+            upstream_fifo=self._vppb_upstream_connection.cxl_cache_fifo,
+            downstream_fifo=self._vppb_downstream_connection.cxl_cache_fifo,
             label=self._get_label(),
         )
 
@@ -122,9 +100,6 @@ class DownstreamPortDevice(CxlPortDevice):
             mmio_manager=mmio_manager,
             label=self._get_label(),
         )
-        if self._is_dummy:
-            self._pci_bridge_component.set_port_number(self._dummy_config.vppb_id)
-            self._pci_bridge_component.set_routing_table(self._dummy_config.routing_table)
 
         # Create MMIO register
         cxl_component = CxlDownstreamPortComponent()
@@ -147,10 +122,6 @@ class DownstreamPortDevice(CxlPortDevice):
         config_space_manager.set_register(self._pci_registers)
 
     def _get_label(self) -> str:
-        if self._is_dummy:
-            vcs_str = f"VCS{self._vcs_id}"
-            vppb_str = f"vPPB{self._vppb_index}(DSP)"
-            return f"{vcs_str}:{vppb_str}"
         return f"DSP{self._port_index}"
 
     def _create_message(self, message: str) -> str:
@@ -160,12 +131,7 @@ class DownstreamPortDevice(CxlPortDevice):
     def get_reg_vals(self):
         return self._cxl_io_manager.get_cfg_reg_vals()
 
-    def get_upstream_connection(self) -> CxlConnection:
-        return self._upstream_connection
-
     def set_vppb_index(self, vppb_index: int):
-        if self._is_dummy:
-            raise Exception("Dummy Downstream Port does not support updating the vPPB index")
         self._vppb_index = vppb_index
         self._pci_bridge_component.set_port_number(self._vppb_index)
 
@@ -173,43 +139,20 @@ class DownstreamPortDevice(CxlPortDevice):
         return CXL_COMPONENT_TYPE.DSP
 
     def set_routing_table(self, routing_table: RoutingTable):
-        if self._is_dummy:
-            raise Exception("Dummy Downstream Port does not support updating the routing table")
         self._pci_bridge_component.set_routing_table(routing_table)
-
-    def backup_enumeration_info(self) -> EnumerationInfo:
-        info = EnumerationInfo(
-            secondary_bus=self._pci_registers.pci.secondary_bus_number,
-            subordinate_bus=self._pci_registers.pci.subordinate_bus_number,
-            memory_base=self._pci_registers.pci.memory_base,
-            memory_limit=self._pci_registers.pci.memory_limit,
-        )
-        return info
 
     def get_secondary_bus_number(self):
         return self._pci_registers.pci.secondary_bus_number
 
-    def restore_enumeration_info(self, info: EnumerationInfo):
-        self._pci_registers.write_bytes(
-            REG_ADDR.SECONDARY_BUS_NUMBER.START,
-            REG_ADDR.SECONDARY_BUS_NUMBER.END,
-            info.secondary_bus,
-        )
-        self._pci_registers.write_bytes(
-            REG_ADDR.SUBORDINATE_BUS_NUMBER.START,
-            REG_ADDR.SUBORDINATE_BUS_NUMBER.END,
-            info.subordinate_bus,
-        )
-        self._pci_registers.write_bytes(
-            REG_ADDR.MEMORY_BASE.START,
-            REG_ADDR.MEMORY_BASE.END,
-            info.memory_base,
-        )
-        self._pci_registers.write_bytes(
-            REG_ADDR.MEMORY_LIMIT.START,
-            REG_ADDR.MEMORY_LIMIT.END,
-            info.memory_limit,
-        )
-
     def get_cxl_component(self) -> CxlDownstreamPortComponent:
         return self._cxl_component
+
+    def set_ppb(self, ppb_device: PpbDevice, ppb_bind):
+        self._ppb_device = ppb_device
+        self._ppb_bind = ppb_bind
+
+    def get_ppb_device(self) -> Optional[PpbDevice]:
+        return self._ppb_device
+
+    def get_ppb_bind(self):
+        return self._ppb_bind
