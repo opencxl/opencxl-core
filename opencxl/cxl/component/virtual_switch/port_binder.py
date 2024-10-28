opencxl/opencxl-core

commit c2bac21a9b0822aacadb17b205855852689e4e7c

Implement SLD vPPB-PPB binding (#124)
Co-authored-by: Chanwoo Moon <ixora990919@gmail.com>
Signed-off-by: Chanwoo Moon <ixora990919@gmail.com>
Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
Signed-off-by: Kyeyoon Park <kyeyoon.park@eeum.io>

opencxl/cxl/component/virtual_switch/port_binder.py
@@ -5,82 +5,17 @@
  See LICENSE for details.
 """
 
-from asyncio import Queue, create_task, gather
+from asyncio import create_task, gather
 from dataclasses import dataclass
 from enum import Enum, auto
 from typing import List, Optional
 
+from opencxl.cxl.component.bind_processor import VppbPpbBindProcessor
 from opencxl.cxl.component.cxl_connection import CxlConnection
+from opencxl.cxl.component.virtual_switch.vppb import Vppb
 from opencxl.cxl.device.downstream_port_device import DownstreamPortDevice
 from opencxl.util.async_gatherer import AsyncGatherer
 from opencxl.util.component import RunnableComponent
-
-
-@dataclass
-class BindPair:
-    source: Queue
-    destination: Queue
-
-
-class BindProcessor(RunnableComponent):
-    def __init__(
-        self,
-        vcs_id: int,
-        vppb_id: int,
-        downsteam_connection: CxlConnection,
-        upstream_connection: CxlConnection,
-    ):
-        super().__init__()
-        self._vcs_id = vcs_id
-        self._vppb_id = vppb_id
-        self._dsc = downsteam_connection
-        self._usc = upstream_connection
-
-        self._pairs = [
-            BindPair(self._dsc.cfg_fifo.host_to_target, self._usc.cfg_fifo.host_to_target),
-            BindPair(self._usc.cfg_fifo.target_to_host, self._dsc.cfg_fifo.target_to_host),
-            BindPair(self._dsc.mmio_fifo.host_to_target, self._usc.mmio_fifo.host_to_target),
-            BindPair(self._usc.mmio_fifo.target_to_host, self._dsc.mmio_fifo.target_to_host),
-            BindPair(
-                self._dsc.cxl_mem_fifo.host_to_target,
-                self._usc.cxl_mem_fifo.host_to_target,
-            ),
-            BindPair(
-                self._usc.cxl_mem_fifo.target_to_host,
-                self._dsc.cxl_mem_fifo.target_to_host,
-            ),
-            BindPair(
-                self._dsc.cxl_cache_fifo.host_to_target,
-                self._usc.cxl_cache_fifo.host_to_target,
-            ),
-            BindPair(
-                self._usc.cxl_cache_fifo.target_to_host,
-                self._dsc.cxl_cache_fifo.target_to_host,
-            ),
-        ]
-
-    def _create_message(self, message):
-        message = f"[{self.__class__.__name__}:VCS{self._vcs_id}:vPPB{self._vppb_id}] {message}"
-        return message
-
-    async def _process(self, source: Queue, destination: Queue):
-        while True:
-            packet = await source.get()
-            if packet is None:
-                break
-            await destination.put(packet)
-
-    async def _run(self):
-        tasks = []
-        for pair in self._pairs:
-            task = create_task(self._process(pair.source, pair.destination))
-            tasks.append(task)
-        await self._change_status_to_running()
-        await gather(*tasks)
-
-    async def _stop(self):
-        for pair in self._pairs:
-            await pair.source.put(None)
 
 
 class BIND_STATUS(Enum):
@@ -91,30 +26,38 @@ class BIND_STATUS(Enum):
 
 @dataclass
 class BindSlot:
-    vppb_connection: CxlConnection
+    vppb: Vppb
     status: BIND_STATUS = BIND_STATUS.INIT
-    processor: Optional[BindProcessor] = None
+    processor: Optional[VppbPpbBindProcessor] = None
     dsp: Optional[DownstreamPortDevice] = None
 
 
 class PortBinder(RunnableComponent):
-    def __init__(self, vcs_id: int, vppb_connections: List[CxlConnection]):
+    def __init__(self, vcs_id: int, vppbs: List[Vppb]):
         super().__init__()
         self._vcs_id = vcs_id
-        self._vppb_connections = vppb_connections
+        self._vppbs = vppbs
         self._bind_slots: List[BindSlot] = []
         self._async_gatherer = AsyncGatherer()
-        for vppb_connection in self._vppb_connections:
+        for vppb in self._vppbs:
             bind_slot = BindSlot(
-                vppb_connection=vppb_connection,
+                vppb=vppb,
             )
             self._bind_slots.append(bind_slot)
+
+        # TODO: dummy is only for keeping PortBinder running as dynamic binding (bind_vppb())
+        # won't be called at this moment. This will be removed once dynamic binding is implemented.
+        self._dummy = VppbPpbBindProcessor(self._vcs_id, 0, CxlConnection(), CxlConnection())
 
     def _create_message(self, message):
         message = f"[{self.__class__.__name__}:VCS{self._vcs_id}] {message}"
         return message
 
+    # TODO: make bind, unbind functional with dynamic binding
     async def bind_vppb(self, dsp_device: DownstreamPortDevice, vppb_index: int):
+        # TODO: L48
+        if self._dummy is not None:
+            self._async_gatherer.add_task(self._dummy.run())
         if vppb_index >= len(self._bind_slots) or vppb_index < 0:
             raise Exception("vppb_index is out of bound")
 
@@ -125,19 +68,21 @@ class PortBinder(RunnableComponent):
         # TODO: Get config space from dummy and store in PPB
         if bind_slot.processor is not None:
             await bind_slot.processor.stop()
-            info = bind_slot.dsp.backup_enumeration_info()
-            dsp_device.restore_enumeration_info(info)
 
         bind_slot.dsp = dsp_device
-        downstream_connection = bind_slot.vppb_connection
-        upstream_connection = dsp_device.get_upstream_connection()
-        bind_slot.processor = BindProcessor(
+        bind_slot.vppb = self._vppbs[vppb_index]
+        downstream_connection = bind_slot.vppb.get_downstream_connection()
+        upstream_connection = dsp_device.get_ppb_device().get_upstream_connection()
+        bind_slot.processor = VppbPpbBindProcessor(
             self._vcs_id, vppb_index, downstream_connection, upstream_connection
         )
         self._async_gatherer.add_task(bind_slot.processor.run())
         bind_slot.status = BIND_STATUS.BOUND
 
-    async def unbind_vppb(self, dsp_device: DownstreamPortDevice, vppb_index: int):
+    async def unbind_vppb(self, vppb_index: int):
+        # TODO: L48
+        if self._dummy is not None:
+            self._async_gatherer.add_task(self._dummy.run())
         if vppb_index >= len(self._bind_slots) or vppb_index < 0:
             raise Exception("vppb_index is out of bound")
 
@@ -148,16 +93,16 @@ class PortBinder(RunnableComponent):
         # TODO: Get config space from PPB and store in dummy
         if bind_slot.processor is not None:
             await bind_slot.processor.stop()
-            info = bind_slot.dsp.backup_enumeration_info()
-            dsp_device.restore_enumeration_info(info)
 
-        bind_slot.dsp = dsp_device
-        downstream_connection = bind_slot.vppb_connection
-        upstream_connection = dsp_device.get_upstream_connection()
-        bind_slot.processor = BindProcessor(
-            self._vcs_id, vppb_index, downstream_connection, upstream_connection
-        )
-        self._async_gatherer.add_task(bind_slot.processor.run())
+        # TODO: Fix during dynamic binding implementation
+        # bind_slot.dsp = dsp_device
+        # bind_slot.vppb = self._vppbs[vppb_index]
+        # downstream_connection = bind_slot.vppb.get_downstream_connection()
+        # upstream_connection = dsp_device.get_ppb_device().get_upstream_connection()
+        # bind_slot.processor = VppbPpbBindProcessor(
+        #     self._vcs_id, vppb_index, downstream_connection, upstream_connection
+        # )
+        # self._async_gatherer.add_task(bind_slot.processor.run())
         bind_slot.status = BIND_STATUS.UNBOUND
 
     def get_bind_status(self, vppb_index: int) -> BIND_STATUS:
@@ -182,8 +127,8 @@ class PortBinder(RunnableComponent):
     def get_bind_slots(self):
         return self._bind_slots
 
-    def get_vppb_connections(self):
-        return self._vppb_connections
+    def get_vppbs(self):
+        return self._vppbs
 
     async def _run(self):
         await self._change_status_to_running()
@@ -194,4 +139,5 @@ class PortBinder(RunnableComponent):
         for slot in self._bind_slots:
             if slot.processor is not None:
                 tasks.append(create_task(slot.processor.stop()))
+        tasks.append(create_task(self._dummy.stop()))
         await gather(*tasks)

opencxl/cxl/component/bind_processor.py
@@ -1,0 +1,99 @@
+"""
+ Copyright (c) 2024, Eeum, Inc.
+
+ This software is licensed under the terms of the Revised BSD License.
+ See LICENSE for details.
+"""
+
+from asyncio import Queue, create_task, gather
+from dataclasses import dataclass
+
+from opencxl.cxl.component.cxl_connection import CxlConnection
+from opencxl.util.component import RunnableComponent
+
+
+@dataclass
+class BindPair:
+    source: Queue
+    destination: Queue
+
+
+class GenericBindProcessor(RunnableComponent):
+    def __init__(
+        self,
+        downsteam_connection: CxlConnection,
+        upstream_connection: CxlConnection,
+    ):
+        super().__init__()
+        self._dsc = downsteam_connection
+        self._usc = upstream_connection
+
+        self._pairs = [
+            BindPair(self._dsc.cfg_fifo.host_to_target, self._usc.cfg_fifo.host_to_target),
+            BindPair(self._usc.cfg_fifo.target_to_host, self._dsc.cfg_fifo.target_to_host),
+            BindPair(self._dsc.mmio_fifo.host_to_target, self._usc.mmio_fifo.host_to_target),
+            BindPair(self._usc.mmio_fifo.target_to_host, self._dsc.mmio_fifo.target_to_host),
+            BindPair(
+                self._dsc.cxl_mem_fifo.host_to_target,
+                self._usc.cxl_mem_fifo.host_to_target,
+            ),
+            BindPair(
+                self._usc.cxl_mem_fifo.target_to_host,
+                self._dsc.cxl_mem_fifo.target_to_host,
+            ),
+            BindPair(
+                self._dsc.cxl_cache_fifo.host_to_target,
+                self._usc.cxl_cache_fifo.host_to_target,
+            ),
+            BindPair(
+                self._usc.cxl_cache_fifo.target_to_host,
+                self._dsc.cxl_cache_fifo.target_to_host,
+            ),
+        ]
+
+    def _create_message(self, message):
+        message = f"[{self.__class__.__name__}] {message}"
+        return message
+
+    async def _process(self, source: Queue, destination: Queue):
+        while True:
+            packet = await source.get()
+            if packet is None:
+                break
+            await destination.put(packet)
+
+    async def _run(self):
+        tasks = []
+        for pair in self._pairs:
+            task = create_task(self._process(pair.source, pair.destination))
+            tasks.append(task)
+        await self._change_status_to_running()
+        await gather(*tasks)
+
+    async def _stop(self):
+        for pair in self._pairs:
+            await pair.source.put(None)
+
+
+class PpbDspBindProcessor(GenericBindProcessor):
+    # vcs_id and vppb_id are not needed in PPB-DSP relation
+    def _create_message(self, message):
+        message = f"[{self.__class__.__name__}] {message}"
+        return message
+
+
+class VppbPpbBindProcessor(GenericBindProcessor):
+    def __init__(
+        self,
+        vcs_id: int,
+        vppb_id: int,
+        downsteam_connection: CxlConnection,
+        upstream_connection: CxlConnection,
+    ):
+        super().__init__(downsteam_connection, upstream_connection)
+        self._vcs_id = vcs_id
+        self._vppb_id = vppb_id
+
+    def _create_message(self, message):
+        message = f"[{self.__class__.__name__}:VCS{self._vcs_id}:vPPB{self._vppb_id}] {message}"
+        return message

opencxl/cxl/component/physical_port_manager.py
@@ -11,8 +11,10 @@ from typing import List, Dict, Optional, cast
 
 from opencxl.cxl.device.port_device import CxlPortDevice
 from opencxl.cxl.device.upstream_port_device import UpstreamPortDevice
+from opencxl.cxl.device.pci_to_pci_bridge_device import PpbDevice
 from opencxl.cxl.device.downstream_port_device import DownstreamPortDevice
 from opencxl.cxl.device.config.logical_device import SingleLogicalDeviceConfig
+from opencxl.cxl.component.bind_processor import PpbDspBindProcessor
 from opencxl.cxl.component.switch_connection_manager import SwitchConnectionManager
 from opencxl.cxl.component.cxl_component import (
     PORT_TYPE,
@@ -45,14 +47,26 @@ class PhysicalPortManager(RunnableComponent):
     ):
         super().__init__()
         self._port_devices: List[CxlPortDevice] = []
+        self._ppb_devices: List[PpbDevice] = []
+        self._ppb_binds: List[PpbDspBindProcessor] = []
+
         self._switch_connection_manager = switch_connection_manager
         self._device_configs = device_configs
         for port_index, port_config in enumerate(port_configs):
             transport_connection = self._switch_connection_manager.get_cxl_connection(port_index)
             if port_config.type == PORT_TYPE.USP:
                 self._port_devices.append(UpstreamPortDevice(transport_connection, port_index))
+                self._ppb_binds.append(None)
             else:
-                self._port_devices.append(DownstreamPortDevice(transport_connection, port_index))
+                physical_port = DownstreamPortDevice(transport_connection, port_index)
+                ppb = PpbDevice(port_index)
+                self._port_devices.append(physical_port)
+                self._ppb_devices.append(ppb)
+                bind = PpbDspBindProcessor(
+                    ppb.get_downstream_connection(), physical_port.get_transport_connection()
+                )
+                self._ppb_binds.append(bind)
+                physical_port.set_ppb(ppb, bind)
 
     def get_port_device(self, port_index: int) -> CxlPortDevice:
         if port_index < 0 or port_index >= len(self._port_devices):
@@ -64,6 +78,12 @@ class PhysicalPortManager(RunnableComponent):
 
     def get_port_devices(self) -> List[CxlPortDevice]:
         return self._port_devices
+
+    def get_ppb_devices(self) -> List[PpbDevice]:
+        return self._ppb_devices
+
+    def get_ppb_binds(self) -> List[PpbDspBindProcessor]:
+        return self._ppb_binds
 
     def get_usp_hdm_decoder_count(self) -> int:
         hdm_decoder_count = 0
@@ -112,6 +132,14 @@ class PhysicalPortManager(RunnableComponent):
         for port_device in self._port_devices:
             run_tasks.append(create_task(port_device.run()))
             wait_tasks.append(create_task(port_device.wait_for_ready()))
+        for ppb_device in self._ppb_devices:
+            run_tasks.append(create_task(ppb_device.run()))
+            wait_tasks.append(create_task(ppb_device.wait_for_ready()))
+
+        for ppb_bind in self._ppb_binds:
+            if ppb_bind is not None:
+                run_tasks.append(create_task(ppb_bind.run()))
+
         await gather(*wait_tasks)
         await self._change_status_to_running()
         await gather(*run_tasks)
@@ -120,4 +148,9 @@ class PhysicalPortManager(RunnableComponent):
         tasks = []
         for port_device in self._port_devices:
             tasks.append(create_task(port_device.stop()))
+        for ppb_device in self._ppb_devices:
+            tasks.append(create_task(ppb_device.stop()))
+        for ppb_bind in self._ppb_binds:
+            if ppb_bind is not None:
+                tasks.append(create_task(ppb_bind.stop()))
         await gather(*tasks)

opencxl/cxl/component/virtual_switch/downstream_vppb.py
@@ -1,0 +1,50 @@
+"""
+ Copyright (c) 2024, Eeum, Inc.
+
+ This software is licensed under the terms of the Revised BSD License.
+ See LICENSE for details.
+"""
+
+from opencxl.cxl.component.common import CXL_COMPONENT_TYPE
+from opencxl.cxl.component.virtual_switch.routing_table import RoutingTable
+from opencxl.cxl.component.cxl_bridge_component import (
+    CxlDownstreamPortComponent,
+)
+from opencxl.cxl.component.virtual_switch.vppb import Vppb
+
+
+# DownstreamVppb class will have many similar methods to DownstreamPortDevice class
+# pylint: disable=duplicate-code
+class DownstreamVppb(Vppb):
+    def __init__(self, vppb_index: int, vcs_id: int):
+        super().__init__()
+        self._vppb_index = vppb_index
+        self._vcs_id = vcs_id
+
+    def _get_label(self) -> str:
+        vcs_str = f"VCS{self._vcs_id}"
+        vppb_str = f"vPPB{self._vppb_index}(DSP)"
+        return f"{vcs_str}:{vppb_str}"
+
+    def _create_message(self, message: str) -> str:
+        message = f"[{self.__class__.__name__}:{self._get_label()}] {message}"
+        return message
+
+    def get_reg_vals(self):
+        return self._cxl_io_manager.get_cfg_reg_vals()
+
+    def set_vppb_index(self, vppb_index: int):
+        self._vppb_index = vppb_index
+        self._pci_bridge_component.set_port_number(self._vppb_index)
+
+    def get_device_type(self) -> CXL_COMPONENT_TYPE:
+        return CXL_COMPONENT_TYPE.DSP
+
+    def set_routing_table(self, routing_table: RoutingTable):
+        self._pci_bridge_component.set_routing_table(routing_table)
+
+    def get_secondary_bus_number(self):
+        return self._pci_registers.pci.secondary_bus_number
+
+    def get_cxl_component(self) -> CxlDownstreamPortComponent:
+        return self._cxl_component

commit 6433d36dbf14cc8ca0b2d45ad0a65fa399a2072c

Fix multiple VCS initialization failures (#129)
Commit c2bac21a9b08 broke starting multiple VCS'. 

Dummy should only run once. Add a dummy flag to avoid multiple
instance being initialized. This is a workaround. It will be removed
once dynamic binding is implemented.

Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
Signed-off-by: Kyeyoon Park <kyeyoon.park@eeum.io>

opencxl/cxl/component/virtual_switch/port_binder.py
@@ -48,6 +48,7 @@ class PortBinder(RunnableComponent):
         # TODO: dummy is only for keeping PortBinder running as dynamic binding (bind_vppb())
         # won't be called at this moment. This will be removed once dynamic binding is implemented.
         self._dummy = VppbPpbBindProcessor(self._vcs_id, 0, CxlConnection(), CxlConnection())
+        self._dummy_initialized = False
 
     def _create_message(self, message):
         message = f"[{self.__class__.__name__}:VCS{self._vcs_id}] {message}"
@@ -56,8 +57,10 @@ class PortBinder(RunnableComponent):
     # TODO: make bind, unbind functional with dynamic binding
     async def bind_vppb(self, dsp_device: DownstreamPortDevice, vppb_index: int):
         # TODO: L48
-        if self._dummy is not None:
+        if self._dummy is not None and not self._dummy_initialized:
             self._async_gatherer.add_task(self._dummy.run())
+            await self._dummy.wait_for_ready()
+            self._dummy_initialized = True
         if vppb_index >= len(self._bind_slots) or vppb_index < 0:
             raise Exception("vppb_index is out of bound")
 
@@ -81,8 +84,10 @@ class PortBinder(RunnableComponent):
 
     async def unbind_vppb(self, vppb_index: int):
         # TODO: L48
-        if self._dummy is not None:
+        if self._dummy is not None and not self._dummy_initialized:
             self._async_gatherer.add_task(self._dummy.run())
+            await self._dummy.wait_for_ready()
+            self._dummy_initialized = True
         if vppb_index >= len(self._bind_slots) or vppb_index < 0:
             raise Exception("vppb_index is out of bound")
 